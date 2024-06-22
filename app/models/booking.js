821a25ead98906257f5f5b207a83bed8495b99ed
Sean <seangores@gmail.com>
2024-06-21 19:11:01 -0600

    fix: activity page

---
+++ app/models/booking.js
@@ -0,0 +1,12 @@
+import Model, {attr} from "@ember-data/model";
+
+console.log(attr);
+
+export default class BookingModel extends Model {
+	@attr("string") activityId;
+	@attr("string") reservationStatus;
+	@attr("string") date;
+	@attr("string") time;
+	@attr tickets; // array of { ticketId, reservationStatus }
+	@attr primaryGuest; // object { name, email, phone }
+}
